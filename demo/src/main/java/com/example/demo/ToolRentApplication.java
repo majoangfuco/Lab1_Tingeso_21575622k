b8cb839dfa5c0472af17a6a96c4ce5df15f16216
Maria_Fu_Co <maria.fuentes.cof@usach.cl>
2025-08-28 14:30:41 -0400

    proyecto iniciado

---
+++ demo/src/main/java/com/example/demo/ToolRentApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolRentApplication.class, args);
+	}
+
+}
